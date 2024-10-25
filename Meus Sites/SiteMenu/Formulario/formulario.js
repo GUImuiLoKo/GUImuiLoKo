24f77c5432dba02a4f8d32ea12d795ae977d7864
GUImuiLoKo <guilhermeoliveirag12346@gmail.com>
2024-10-25 10:26:45 -0500

    gggggggg

---
+++ Meus Sites/SiteMenu/Formulario/formulario.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// while(true){
+    
+//     var conta = String(prompt("digite seu nome para criar sua conta: "))
+//     var senha = String(prompt("crie sua senha: "))
+    
+//     alert("Agora vamos logar na sua conta")
+    
+//     var conta1 = String(prompt("digite sua conta: "))
+//     var senha1 = String(prompt("digite sua senha: "))
+    
+//     if(conta1 == conta && senha1 == senha){
+//         alert("login concluido")
+//     break
+//     }
+//     else {
+//         alert("falha no login")
+//     }
+//     }
--- Meus Sites/SiteMenu/Formulario/formulario.php
+++ Meus Sites/SiteMenu/Formulario/formulario.php
@@ -21,6 +21,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     <script src="formulario.js" defer></script>
 </head>
 <body>
+
 <div id="fundo">
 
     <h1>FORMULÁRIO DE CADRASTRO</h1>
